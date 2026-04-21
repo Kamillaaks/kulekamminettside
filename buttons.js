13350541915bb8ff59e16d42d68d7f808354be95
Kamillaaks <kammi.ar@hotmail.com>
2026-04-21 19:24:04 +0200

    JS

---
+++ buttons.js
@@ -0,0 +1,34 @@
+const projects = document.querySelectorAll(".project-card");  
+let current = 0;
+
+function showProject(index) {
+    for (let i= 0; i < projects.length; i++) {
+        if (i === index) {
+            projects[i].style.display = "block";
+        } else {
+            projects[i].style.display = "none";
+        }
+    }
+}
+
+document.getElementById("next").onclick = function () {
+    current = current + 1;
+
+    if (current >= projects.length) {
+        current = 0;
+    }
+
+    showProject(current);
+}
+
+document.getElementById("prev").onclick = function () {
+    current = current - 1;
+
+    if (current < 0) {
+        current = projects.lenght - 1;
+    }
+
+    showProject(current);
+}
+
+showProject(current);
